51f92e17ae0323dd001bb4f78f359625bac26a6d
Teddy Economou <teddyecon@gmail.com>
2023-03-07 14:17:39 -0500

    Theme Relationship Built

--- sql_words.py
+++ sql_words.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, create_engine, ForeignKey, Table
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey, PrimaryKeyConstraint
 from sqlalchemy.orm import Session, relationship, backref 
 import pandas as pd
 
@@ -7,18 +7,58 @@ Base = declarative_base()
 
 class Easy_Words(Base):
     __tablename__ = 'easy'
+    __table_args__ = (PrimaryKeyConstraint('id'),)
     id = Column(Integer(), primary_key=True)
-    words = Column(String())
+    easy_words = Column(String())
+    themes = relationship('Themes', backref=backref('eword'))
+    
+
+    def __repr__(self):
+        return f'Easy_Words(id={self.id}),' + \
+            f'easy_words={self.easy_words}'        
 
 class Medium_Words(Base):
     __tablename__ = 'medium'
+    __table_args__ = (PrimaryKeyConstraint('id'),)
     id = Column(Integer(), primary_key=True)
-    words = Column(String())
+    medium_words = Column(String())
+    themes = relationship('Themes', backref=backref('mword'))
+
+    def __repr__(self):
+        return f'Medium_Words(id={self.id}),' + \
+            f'medium_words={self.medium_words}' 
 
 class Difficult_Words(Base):
     __tablename__ = 'difficult'
+    __table_args__ = (PrimaryKeyConstraint('id'),)
     id = Column(Integer(), primary_key=True)
-    words = Column(String())
+    difficult_words = Column(String())
+    themes = relationship('Themes', backref=backref('dword'))
+
+    def __repr__(self):
+        return f'Difficult_Words(id={self.id}),' + \
+            f'difficult_words={self.difficult_words}' 
+
+class Themes(Base):
+    __tablename__ = 'themes'
+    id = Column(Integer(), primary_key=True)
+    themes = Column(String())
+    easy_words_id = Column(Integer(), ForeignKey("easy.id"))
+    medium_words_id = Column(Integer(), ForeignKey("medium.id"))
+    difficult_words_id = Column(Integer(), ForeignKey("difficult.id"))
+
+    def __repr__(self):
+        return f'Easy_Words(id={self.id}),' + \
+            f'eword={self.easy_words}'
+
+    def __repr__(self):
+        return f'Medium_Words(id={self.id}),' + \
+            f'mword={self.medium_words}'
+
+    def __repr__(self):
+        return f'Difficult_Words(id={self.id}),' + \
+            f'difficult_words={self.difficult_words}' 
+    
 
 engine = create_engine('sqlite:///word_list.db')
 # Base.metadata.create_all(engine)
@@ -26,18 +66,20 @@ word_list = pd.read_csv('words_sidtest.csv')
 with Session(engine) as session: 
     for each_word in word_list.values.flatten():
         if len(each_word) < 5:
-            word = Easy_Words(
-                words = each_word
+            eword = Easy_Words(
+                easy_words = each_word
             )
         if len(each_word) > 5 and len(each_word) < 8:
-                word = Medium_Words(
-            words = each_word
+                mword = Medium_Words(
+            medium_words = each_word
         )
 
         elif len(each_word) > 9:
-            word = Difficult_Words(
-                words = each_word
+            dword = Difficult_Words(
+                difficult_words = each_word
                 )
 
-        session.add(word)
+    session.add(eword)
+    session.add(mword)
+    session.add(dword)
     session.commit()
