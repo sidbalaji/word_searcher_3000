b310a7adaa2cce1424fc80558809b0369146b77e
Siddarth Balaji <sid.balaji@berkeley.edu>
2023-03-08 13:03:18 -0500

    sql backend linked with wordsearch frontend. some cleanup of main.py required

--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@ from grid import Grid
 from word import Word
 from colorama import Fore
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session , session_maker
-
+from sqlalchemy.orm import Session
+ 
 from sqlalchemy import Table
 from sqlalchemy import inspect
-
+from sql_words import Easy_Words, Medium_Words, Difficult_Words
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
@@ -47,38 +47,58 @@ def check_guess(guess,word_list, grid , start_color_regex = Fore.GREEN, end_colo
     #we want to return word list because we modify if each word found is true
     return word_list
     
-def run_evertyhing(difficulty):
-
-    return difficulty
-
-if __name__ == '__main__':
-    
-
+def run_evertyhing(difficulty, grid_size = 15, num_words = 5):
+    word_list = []
     engine = create_engine("sqlite:///word_list.db")
-    
+
+    if difficulty.lower() == 'easy':
+        tbl = Easy_Words
+    elif difficulty.lower() == 'medium':
+        tbl = Medium_Words
+
     with Session(engine) as session:
-        session.query()
+        for n in range(num_words):
+            rand = random.randrange(0, session.query(tbl).count())
+            row = session.query(tbl)[rand]
+            word_list.append(row)
+    
+    if difficulty.lower() == 'easy':  
+        word_list = [Word(_.easy_words) for _ in word_list]
+    elif difficulty.lower() == 'medium':
+        word_list = [Word(_.medium_words) for _ in word_list]
 
-    ###################################
-    # The below two lines are inplace of SQL input
-    #####################################
-    word_list = ["able","about","above","accept", "momhole"]
-
-    word_list = [Word(_) for _ in word_list]
     
     #initializing that all words are not found (default is false)
     for word in word_list:
         word.found = False
 
     #geting the grid to actually initialize
-    testGrid = Grid(15)
+    testGrid = Grid(grid_size)
     testGrid.add_words(word_list)
 
-        
-    #present table    
     print_grid(testGrid.matrix)
-    
+    print([_.text for _ in word_list])        
     while not all([_.found for _ in word_list]):
         guess = input('Enter a word! : ')
         word_list = check_guess(guess, word_list, testGrid)
+        print([_.text for _ in word_list])        
 
+
+    
+    return 0
+
+if __name__ == '__main__':
+    run_evertyhing('medium', grid_size=20, num_words=10)
+
+    
+        
+        
+
+    ###################################
+    # The below two lines are inplace of SQL input
+    #####################################
+
+    
+
+        
+    #present table    
