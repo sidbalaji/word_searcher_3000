0585e9f6889431dd2070ca4c0265f785dbc9d17d
Teddy Economou <teddyecon@gmail.com>
2023-03-09 11:45:32 -0500

    Easter Egg Added

--- src/main.py
+++ src/main.py
@@ -4,25 +4,53 @@ from word import Word
 from colorama import Fore
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
- 
-from sqlalchemy import Table
-from sqlalchemy import inspect
-from sql_words import Easy_Words, Medium_Words, Difficult_Words
+import os
+from sql_words import Easy_Words, Medium_Words, Difficult_Words, Pokemon
 import argparse
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 found_color_regex = "\033[1;33m"
-def heading():
-	print("┌────────────────────┐")
-	print("│ Word Searcher 3000 │")
-	print("└────────────────────┘")
+normal_header = """
+             .--.            .---.           .-.
+         .---|--|    .-.     | W |  .------. |~|    .--.
+      .--|===|Se|----|_|--.__| O |--|::::::| |~|-==-|==|---.
+      |~~| W |ar|====| |~~|--| R |--| WILL |_|~|    |  |___|-.
+      |  | O |ch|====| |==|  | D |  |::::::|=| |    |  |---|=|
+      |  | R |er|3000|_|__|  | S |__|BE HAD| | |    |  |___| |
+      |~~| D |--|====|~|~~|==|~~~|--|:8==D:|=|~|----|==|---|=|
+      ^--^---'--^----^-^--^--^---'--^------^-^-^-==-^--^---^-'
+    
+"""
+pokemon_header = """
+                                  ,'\\
+    _.----.        ____         ,'  _\   ___    ___     ____
+_,-'       `.     |    |  /`.   \,-'    |   \  /   |   |    \  |`.
+\      __    \    '-.  | /   `.  ___    |    \/    |   '-.   \ |  |
+ \.    \ \   |  __  |  |/    ,','_  `.  |          | __  |    \|  |
+   \    \/   /,' _`.|      ,' / / / /   |          ,' _`.|     |  |
+    \     ,-'/  /   \    ,'   | \/ / ,`.|         /  /   \  |     |
+     \    \ |   \_/  |   `-.  \    `'  /|  |    ||   \_/  | |\    |
+      \    \ \      /       `-.`.___,-' |  |\  /| \      /  | |   |
+       \    \ `.__,'|  |`-._    `|      |__| \/ |  `.__,'|  | |   |
+        \_.-'       |__|    `-._ |              '-.|     '-.| |   |
+                                `'                            '-._|
+"""
 
+def heading(pokemon_heading):
+    # print(text)
+    if pokemon_heading: 
+        print(pokemon_header)
+    else:
+        print(normal_header)
+    # print("┌────────────────────────┐")
+	# print("│ Pokemon Searcher 3000 │")
+	# print("└────────────────────────┘")
 
-def print_grid(grid_list):
-    heading()
+def print_grid(grid_list,pokemon_heading):
+    heading(pokemon_heading)
     #starting the text with all back colors
-    print_list = f"{Fore.BLACK}"
+    print_list = f"{Fore.WHITE}"
     test_list = ""
     for row in grid_list:
         print_list = print_list + "\n"
@@ -35,7 +63,7 @@ def print_grid(grid_list):
 
     
 
-def check_guess(guess,word_list, grid , start_color_regex = Fore.GREEN, end_color_regex = Fore.BLACK):
+def check_guess(guess,word_list, grid,pokemon_heading , start_color_regex = Fore.GREEN, end_color_regex = Fore.WHITE):
     for each_word in word_list:
         if guess.lower() == each_word.text.lower():
             each_word.found = True
@@ -43,7 +71,7 @@ def check_guess(guess,word_list, grid , start_color_regex = Fore.GREEN, end_colo
                 grid.matrix[i][j] = f"{start_color_regex}{grid.matrix[i][j]}{end_color_regex}"
 
 
-    print_grid(grid.matrix)
+    print_grid(grid.matrix, pokemon_heading)
     
     #we want to return word list because we modify if each word found is true
     return word_list
@@ -58,6 +86,10 @@ def run_evertyhing(difficulty, grid_size = 15, num_words = 5):
         tbl = Medium_Words
     elif difficulty.lower() == 'difficult':
         tbl = Difficult_Words
+    elif difficulty.lower() == 'pokemon':
+        tbl = Pokemon
+    else:
+        tbl = Pokemon
 
     with Session(engine) as session:
         for n in range(num_words):
@@ -71,9 +103,10 @@ def run_evertyhing(difficulty, grid_size = 15, num_words = 5):
         word_list = [Word(_.medium_words) for _ in word_list]
     elif difficulty.lower() == 'difficult':
         word_list = [Word(_.difficult_words) for _ in word_list]
-
-
-    
+    elif difficulty.lower() == 'pokemon':
+        word_list = [Word(_.pokemon_names) for _ in word_list]
+    else:
+        word_list = [Word(_.pokemon_names) for _ in word_list]
     #initializing that all words are not found (default is false)
     for word in word_list:
         word.found = False
@@ -82,12 +115,16 @@ def run_evertyhing(difficulty, grid_size = 15, num_words = 5):
     testGrid = Grid(grid_size)
     testGrid.add_words(word_list)
 
-    print_grid(testGrid.matrix)
+    pokemon_heading = False if difficulty.lower() in ['easy', 'medium', 'difficult'] else True
+    print_grid(testGrid.matrix , pokemon_heading = pokemon_heading)
     print([_.text for _ in word_list])        
     while not all([_.found for _ in word_list]):
         guess = input('Enter a word! : ')
-        word_list = check_guess(guess, word_list, testGrid)
-        print([_.text for _ in word_list])        
+        os.system("clear")
+
+        word_list = check_guess(guess, word_list, testGrid, pokemon_heading)
+        print([_.text for _ in word_list])
+
 
 
     
@@ -97,14 +134,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     
 
-    parser.add_argument("difficulty", type=str)
-    parser.add_argument("--grid_size", type=int)
-    parser.add_argument("--num_words", type=int)
+    parser.add_argument("difficulty", type=str, default='pokemon')
+    parser.add_argument("--grid_size", type=int, default=25)
+    parser.add_argument("--num_words", type=int, default=8)
 
     args = parser.parse_args()
-    print("--------------------------------")
     print(args)
-    print("--------------------------------")
+
     run_evertyhing(args.difficulty, args.grid_size, args.num_words)
 
     
--- src/sql_words.py
+++ src/sql_words.py
@@ -15,7 +15,13 @@ class Easy_Words(Base):
 
     def __repr__(self):
         return f'Easy_Words(id={self.id}),' + \
-            f'easy_words={self.easy_words}'        
+            f'easy_words={self.easy_words}'
+
+class Pokemon(Base):
+    __tablename__ = 'pokemon'
+    __table_args__ = (PrimaryKeyConstraint('id'),)
+    id = Column(Integer(), primary_key=True)
+    pokemon_names = Column(String())        
 
 class Medium_Words(Base):
     __tablename__ = 'medium'
@@ -61,25 +67,29 @@ class Themes(Base):
     
 
 engine = create_engine('sqlite:///word_list.db')
-# Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
 word_list = pd.read_csv('words_sidtest.csv')
+pokemon_list = pd.read_csv('pokemon.csv')
 with Session(engine) as session: 
-    for each_word in word_list.values.flatten():
-        if len(each_word) < 5:
-            eword = Easy_Words(
-                easy_words = each_word
-            )
-        if len(each_word) > 5 and len(each_word) < 8:
-                mword = Medium_Words(
-            medium_words = each_word
-        )
+    for pokemon in pokemon_list.Name.values.flatten():
+        pkm = Pokemon(pokemon_names = pokemon)
+        session.add(pkm)
+    # for each_word in word_list.values.flatten():
+    #     if len(each_word) < 5:
+    #         eword = Easy_Words(
+    #             easy_words = each_word
+    #         )
+    #     if len(each_word) > 5 and len(each_word) < 8:
+    #             mword = Medium_Words(
+    #         medium_words = each_word
+    #     )
 
-        elif len(each_word) > 9:
-            dword = Difficult_Words(
-                difficult_words = each_word
-                )
+    #     elif len(each_word) > 9:
+    #         dword = Difficult_Words(
+    #             difficult_words = each_word
+    #             )
 
-    session.add(eword)
-    session.add(mword)
-    session.add(dword)
+    # session.add(eword)
+    # session.add(mword)
+    # session.add(dword)
     session.commit()
